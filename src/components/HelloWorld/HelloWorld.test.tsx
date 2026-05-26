060b36b7286fcbb84734ba33d8b1d976000d178c
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-26 20:02:16 +0400

    project setup

--- src/components/HelloWorld/HelloWorld.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { HelloWorld } from './HelloWorld'
-
-describe('HelloWorld', () => {
-  it('renders the message', () => {
-    render(<HelloWorld message="Hello, World!" />)
-    expect(screen.getByText('Hello, World!')).toBeInTheDocument()
-  })
-})
--- src/components/HelloWorld/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from 'antd'
-
-interface HelloWorldProps {
-  message: string
-}
-
-export function HelloWorld({ message }: HelloWorldProps) {
-  return <Typography.Text>{message}</Typography.Text>
-}
