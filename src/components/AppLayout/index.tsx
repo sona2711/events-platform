b9996c249d34c371ce9002f7fe84673fc524de2a
Parandzem-Tad <tadevosyan.parandzem@mail.ru>
2026-05-28 12:10:39 +0400

    feat:Complete Hero section

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+// import { Header } from '@/components/Header'
 import styles from './styles.module.css'
 
 export function AppLayout() {
   return (
     <div className={styles.layout}>
-      <Header />
+      {/* <Header /> */}
       <main className={styles.main}>
         <Outlet />
       </main>
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,10 +1,4 @@
-import { PagePlaceholder } from '@/components/PagePlaceholder'
-
+import { Hero } from '@/components/Hero'
 export function MainPage() {
-  return (
-    <PagePlaceholder
-      title="All Events"
-      description="Discover upcoming gatherings, talks, and community events across Yerevan."
-    />
-  )
+  return <Hero />
 }
